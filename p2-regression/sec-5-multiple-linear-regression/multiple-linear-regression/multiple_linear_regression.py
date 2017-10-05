719d09f8637235b9bf2db564f9ed702a65822b52
Dawson Mortenson <dawson.mortenson@gmail.com>
2017-10-04 21:44:37 -0700

    finally wrap up p2, sec-5 and add my module for preforming backwards elimination!

--- p2-regression/sec-5-multiple-linear-regression/multiple-linear-regression/multiple_linear_regression.py
+++ p2-regression/sec-5-multiple-linear-regression/multiple-linear-regression/multiple_linear_regression.py
@@ -33,8 +33,35 @@ regressor.fit(x_train, y_train)
 # Predicts the results using the regressor model
 y_pred = regressor.predict(x_test)
 
-import statsmodels.formula.api as sm
+# Building the optimal model using Backward Elimination
+
+#import statsmodels.formula.api as sm
+# Appends a column of 1s to the matrix to account for the X(B0) value. Otherwise the model doesn't know it exists
+# To add to the beginning of the matrix we put the array of ones as the parameter "arr"
 x = np.append(arr = np.ones((50, 1)).astype(int), values = x, axis = 1)
-x_opt = x[:, [i for i in range(0, 6)]] # nifty range to list codebit
-regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+
+#################### OLD WAY WITHOUT AWESOME METHOD ##########################
+#x_opt = x[:, [i for i in range(0, 6)]] # nifty range to list codebit
+## Endog = y and Exog = x. Note: exog doesn't assume a y-intercept. We had to explicity add it above
+#regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+## P-values are the measure of importance.
+#regressor_OLS.summary()
+#x_opt = x[:, [0,1,3,4,5]]
+#regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+#regressor_OLS.summary()
+#x_opt = x[:, [0,3,5]]
+#regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+#regressor_OLS.summary()
+#x_opt = x[:, [0,3]]
+#regressor_OLS = sm.OLS(endog = y, exog = x_opt).fit()
+#regressor_OLS.summary()
+##############################################################################
+
+# I wrote a backwards elimination method and put it in a custom module!
+# Runs a while loop remove unsignificant values
+from eliminations import backwards_elimination
+
+# Takes the x/y columns, the width of the feature set, and the p signifigance value
+regressor_OLS = backwards_elimination(x,y,5)
+
 regressor_OLS.summary()
